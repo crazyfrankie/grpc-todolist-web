38e249b39800c6a5295f4532c29e284d85cce75a
crazyfrank <axu9417@gmail.com>
2025-06-22 01:40:27 +0800

    fix(home):rolling bar

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,26 @@
 @import '@/assets/scss/main.scss';
 
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
+  /* 隐藏滚动条 */
+  overflow-x: hidden;
+  overflow-y: auto;
+  
+  /* Webkit浏览器（Chrome, Safari等） */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  
+  /* Firefox */
+  scrollbar-width: none;
+  
+  /* IE和Edge */
+  -ms-overflow-style: none;
 }
 </style>
